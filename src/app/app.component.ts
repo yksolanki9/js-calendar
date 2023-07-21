ef23795a0ba7260a17f3927e9513bacc42cb871a
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-21 17:19:03 +0530

    Done on call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calendaer';
+  days: string[] = [];
+
+  monthDetails: any[] = [];
+
+  today: number = -1;
+
+  selectedDate: number = -1;
+
+  constructor() {}
+
+  //How many days are there in a month?
+  //Which day it is?
+
+  //42 elements always needed
+  // {
+  //   date: number;
+  //   day: 'MONDAY';
+  // }
+  ngOnInit() {
+    const startOfMonth = dayjs().startOf('month');
+    this.today = dayjs().date();
+    console.log('startOFMonth is', startOfMonth);
+    const daysInMonth = dayjs().daysInMonth();
+
+    this.monthDetails = [];
+    let currentDay = startOfMonth.day();
+
+    for (let i = 0; i < daysInMonth; i++) {
+      this.monthDetails.push({
+        date: i + 1,
+        day: currentDay % 7,
+      });
+      currentDay++;
+    }
+
+    const prefixDays = this.monthDetails[0].day;
+    for (let i = 0; i < prefixDays; i++) {
+      this.monthDetails.unshift({
+        day: 0,
+        date: 0,
+      });
+    }
+
+    console.log('MONTH DETAIUls', this.monthDetails);
+
+    this.days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  }
+
+  dateSelected(date: number) {
+    this.selectedDate = date;
+  }
 }
