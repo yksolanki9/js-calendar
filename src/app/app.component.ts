7db01e07d5468157cc4001a614b01a6f682253c8
yksolanki9 <yashsolanki1709@gmail.com>
2023-08-01 17:59:52 +0530

    Final refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,11 @@ import { Dayjs } from 'dayjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  days: string[] = [];
+  nameOfDays: string[] = [];
 
-  monthDetails: any[] = [];
-
-  selectedDate: Dayjs = dayjs();
+  selectedDate: Dayjs | null = null;
 
   calendarToShow: Dayjs[] = [];
-
-  currentMonth: number = 0;
 
   currentDate: Dayjs = dayjs();
 
@@ -25,56 +21,56 @@ export class AppComponent {
   constructor() {}
 
   ngOnInit() {
-    this.days = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-    this.currentMonth = dayjs().month();
-    this.createMonthCalendar(this.currentMonth);
-
+    this.nameOfDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     this.today = dayjs();
-  }
-
-  setCurrentMonth(val: number){
-    this.currentMonth = val;
-    this.createMonthCalendar(val);
-  }
-
-  createMonthCalendar(currentMonth: number) {
-    console.log('CURRENT MONTH IS', currentMonth, 'and dayjsmonth is', dayjs().month());
-    this.currentDate = dayjs().add(currentMonth - dayjs().month(), 'month');
-    const currentYear = this.currentDate.year();
-
-    const daysInCurrentMonth = dayjs().set('month', currentMonth).daysInMonth();
-    const daysInPreviousMonth = dayjs().set('month', currentMonth - 1).daysInMonth();
-
-    const dayOfWeek = dayjs().set('month', currentMonth).startOf('month').day();
-    //0 to 6
-
-    const currentMonthCalendar = Array.from(Array(daysInCurrentMonth).keys()).map((val) => dayjs(new Date(currentYear, currentMonth, val+1)));
-
-    //Returns [0..6]
-    const prevArray = Array.from(Array(dayOfWeek).keys());
-
-    //Returns [6..0]
-    prevArray.reverse()
-    const prevYear = currentMonth - 1  < 0 ? currentYear - 1 : currentYear;
-
-    //Gives last month's calendar
-    const lastMonthCalendar = prevArray.map((val) => dayjs(new Date(prevYear, currentMonth - 1, daysInPreviousMonth - val)));
-
-    //For next month. Got 42 as google calendar always shows 6 weeks to maintain the same height of calendar
-    const lenOfNextMonth = 42 - (currentMonthCalendar.length + lastMonthCalendar.length);
-
-    const nextYear = currentMonth + 1  > 11 ? currentYear + 1 : currentYear;
-    const nextMonthCalendar = Array.from(Array(lenOfNextMonth).keys()).map((val) => dayjs(new Date(nextYear, currentMonth + 1, val + 1)));
-
-    this.calendarToShow = [...lastMonthCalendar, ...currentMonthCalendar, ...nextMonthCalendar];
-    console.log('calendarToShow', this.calendarToShow);
+    this.createAndShowCalendar();
   }
 
   dateSelected(dayjsObj: Dayjs) {
-    if(dayjsObj.month() !== this.currentMonth) {
-      this.setCurrentMonth(dayjsObj.month());
+    if(dayjsObj.isBefore(this.currentDate, 'month')) {
+      this.changeMonth('PREV');
+    } else if(dayjsObj.isAfter(this.currentDate, 'month')) {
+       this.changeMonth('NEXT');
     }
     this.selectedDate = dayjsObj;
+  }
+
+  changeMonth(month: 'NEXT' | 'PREV') {
+    this.currentDate = this.currentDate.add(month === 'NEXT' ? 1 : -1, 'month') ;
+    this.createAndShowCalendar();
+  }
+
+  createAndShowCalendar() {
+    const daysInCurrentMonth = this.currentDate.daysInMonth();
+    const daysInPreviousMonth = this.currentDate.subtract(1, 'month').daysInMonth();
+    const dayOfStartOfMonth = this.currentDate.startOf('month').day();
+
+    //Create dayjs[] for days in current month
+    const currentMonth = this.currentDate.month();
+    const currentYear = this.currentDate.year();
+    const currentMonthCalendar = Array.from(Array(daysInCurrentMonth).keys()).map((val) => dayjs(new Date(currentYear, currentMonth, val+1)));
+    
+    /*
+    * Create dayjs[] for days in prev month
+    * - First, create an array of size equal to the no. of days of prev month that will be shown
+    * - Then, reverse the array. So [0, 1, .., 6] becomes [6, .., 0]
+    * - With this, we can subtract the days in previous month from each value in this array to get date of prev month
+    */
+    const prevArray = Array.from(Array(dayOfStartOfMonth).keys());
+    prevArray.reverse()
+    const prevYear = currentMonth - 1  < 0 ? currentYear - 1 : currentYear;
+    const prevMonthCalendar = prevArray.map((val) => dayjs(new Date(prevYear, currentMonth - 1, daysInPreviousMonth - val)));
+
+    /*
+    * Create dayjs[] for days in next month
+    * - Subtract the total days from 42 as we'll show 42 days everytime to maintain the height of calendar
+    * - Calculate the next month's calendar using the same procedure
+    */
+    const lenOfNextMonth = 42 - (currentMonthCalendar.length + prevMonthCalendar.length);
+    const nextYear = currentMonth + 1  > 11 ? currentYear + 1 : currentYear;
+    const nextMonthCalendar = Array.from(Array(lenOfNextMonth).keys()).map((val) => dayjs(new Date(nextYear, currentMonth + 1, val + 1)));
+
+    //Combine the arrays to get the calendar that will be shown
+    this.calendarToShow = [...prevMonthCalendar, ...currentMonthCalendar, ...nextMonthCalendar];
   }
 }
